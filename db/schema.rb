55331d6e342fb18ca7c77f9b3b9958755c066add
Patrick Clifford <paddyclifford@gmail.com>
2013-10-16 23:54:03 +0100

    Added hearts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131016191917) do
+ActiveRecord::Schema.define(version: 20131016213124) do
+
+  create_table "hearts", force: true do |t|
+    t.integer "post_id"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
